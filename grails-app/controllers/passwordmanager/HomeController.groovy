7800a16ef600ba55fa8dfd384592b96b3c86d23c
c_rajees <c_rajees@qualcomm.com>
2017-12-16 23:55:12 -0800

    Adding encryption and decryption algorithm

--- grails-app/controllers/passwordmanager/HomeController.groovy
+++ grails-app/controllers/passwordmanager/HomeController.groovy
@@ -4,6 +4,13 @@ class HomeController {
 
     HomeService homeService
 
+    /*
+    *
+    * whenever doing any operation with user/paswd immediatedly email should be triggered.
+    * set flag for email sent or not, date field also
+    * user/paswd details updated date also .. all this should sync together.
+    * */
+
     def index() {
 
         render ( view : "index")
